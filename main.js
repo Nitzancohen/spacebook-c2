a465f98341306c40bd8a11843669b503771061e1
nitzan cohen <nitzancohen105@gmail.com>
2018-08-26 13:27:30 +0300

    exercise 2

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ $('.add-post').click(function() {
     let postText = $('#post-name').val()
     let newPost = {text: postText, id: postID}
     posts.push(newPost)
+    $('#post-name').val("")
     postID++
+    renderPosts()
 })
+
+const renderPosts = function () {
+    $('.posts').empty()
+    for (let post of posts) {
+        let postToPrint = $('<p class="post" data-id="' + post.id + '">' + post.text + '</p>')
+        $('.posts').append(postToPrint)
+    }
+}
