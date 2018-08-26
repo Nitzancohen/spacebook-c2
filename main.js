7f8689299a00747358e404f6760cce8f9ee1121a
nitzan cohen <nitzancohen105@gmail.com>
2018-08-26 13:09:40 +0300

    exercise 1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const posts = []
+let postID = 0
 
+$('.add-post').click(function() {
+    let postText = $('#post-name').val()
+    let newPost = {text: postText, id: postID}
+    posts.push(newPost)
+    postID++
+})
