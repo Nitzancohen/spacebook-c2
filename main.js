c49eae778f1745e9c26de6d6758cdf16bd07c942
nitzan cohen <nitzancohen105@gmail.com>
2018-08-26 21:49:48 +0300

    extension 1

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ let autoPostID = 0
 
 $('.add-post').on ('click', function () {
     let postText = $('#post-name').val()
-    let newPost = {text: postText, id: autoPostID}
-    posts.push(newPost)
-    renderPosts()
-    $('#post-name').val("")
-    autoPostID++
+    if(postText){
+        let newPost = {text: postText, id: autoPostID, comments: []}
+        posts.push(newPost)
+        renderPosts()
+        autoPostID++
+    }
 })
 
 $('.posts').on('click', '.remove', function () {
@@ -21,15 +22,55 @@ $('.posts').on('click', '.remove', function () {
     renderPosts()
 })
 
+$('.posts').on('click', '.comment-btn', function () {
+    let id = $(this).closest('form').closest('p').data('id')
+    let comment = $(this).closest('form').find('.comment').val()
+    let username = $(this).closest('form').find('.username').val()
+    for (let post of posts) {
+        if (post.id == id) {
+            post.comments.push({username: username, comment: comment})
+            break;
+        }
+    }
+    renderPosts()
+})
+
 const renderPosts = function () {
+    $('#post-name').val("")
     $('.posts').empty()
     for (let i=0; i<posts.length; i++) {
         let postToPrint = createPostElement(posts[i])
-        addRemoveButton(postToPrint, i)
+        addRemoveButton(postToPrint)
+        addCommentsForm(postToPrint)
+        addPostComments(postToPrint)
         $('.posts').append(postToPrint)
     }
 }
 
 const createPostElement = function (post) { return $('<p class="post" data-id="' + post.id + '">' + post.text + '</p>') }
 
-const addRemoveButton = function (post, index) { post.append($('<button type="button" class="remove">REMOVE</button>')) }
+const addRemoveButton = function (post) { post.prepend($('<button type="button" class="remove">REMOVE</button>')) }
+
+const addCommentsForm = function (post) {
+    let form = $('<form class="comment-form">' +
+    '<input type="text" class="username" placeholder="Username">' +
+    '<input type="text" class="comment" placeholder="Write a comment...">' +
+    '<button type="button" class="comment-btn">Comment</button><div class="post-comments"></div></form>')
+    post.append(form)
+}
+
+const addPostComments = function (post) {
+    let postId = post.data('id')
+    let comments
+    for (let p of posts) {
+        if (p.id == postId) {
+            comments = p.comments
+            break;
+        }
+    }
+    let commentsDiv = post.find('.post-comments')
+    commentsDiv.empty()
+    for (let comment of comments) {
+        commentsDiv.append('<li>' + comment.username + ' says: "' + comment.comment + '"</li>')
+    }
+}
